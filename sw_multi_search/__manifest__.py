4fb53c26a651e2b2de315dcbbdafb81766a1ec31
Hammam <61102207+RLJO@users.noreply.github.com>
2020-12-05 22:36:10 +0200

    [ADD] app sw_multi_search

---
+++ sw_multi_search/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'SW - Multi Search',
+    'version': '12.0.1.0',
+    'category' : 'General',
+    'summary' : """ Records bulk search on any page/model.
+    """,
+    'description': """
+                Search for records in bulks on any page/model easily.
+                """,
+    'license':  "Other proprietary",
+    'author' : 'Smart Way Business Solutions',
+    'website': 'www.smartway.co',
+    'depends': ['base'],
+    'init_xml': [],
+    'data': [
+             ], 
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    "images":  ['static/description/image.png'],
+    
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
